2c07aa4300aee3a96391fa5830c29a5a5480d849
gabokatta <gkatta@fi.uba.ar>
2025-09-02 20:06:28 -0300

    log fix

--- server/common/bet_handler.py
+++ server/common/bet_handler.py
@@ -16,18 +16,21 @@ class BetHandler:
         if not isinstance(packet, BetPacket):
             return ErrorPacket(ErrorPacket.INVALID_PACKET, "Did not receive correct BetPacket.")
 
+        agency = packet.agency_id
         bets: [Bet] = []
         try:
-            parsed = ProtocolBet.to_domain_list(packet.agency_id, packet.bets)
+            parsed = ProtocolBet.to_domain_list(agency, packet.bets)
             bets.extend(parsed)
         except Exception as e:
-            logging.error(f"action: apuesta_recibida | result: fail | error: {e}")
+            logging.error(f"action: apuesta_recibida | result: fail | client_id:{agency} | error: {e}")
             return ErrorPacket(ErrorPacket.INVALID_BET, "Invalid Bet batch, could not parse.")
 
         try:
             store_bets(bets)
-            logging.info(f"action: apuesta_recibida | result: success | cantidad: {len(bets)}")
+            logging.info(f"action: apuesta_recibida | result: success | cantidad: {len(bets)} | client_id: {agency}")
             return ReplyPacket(len(bets), "STORED")
         except Exception as e:
-            logging.error(f"action: apuesta_recibida | result: fail | cantidad: {len(bets)} | error: {e}")
+            logging.error(
+                f"action: apuesta_recibida | result: fail | cantidad: {len(bets)} | client_id_ {agency} | error: {e}"
+            )
             return ErrorPacket(ErrorPacket.INVALID_BET, f"Internal server error processing batch of {len(bets)} bets")
