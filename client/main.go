a0950b3a6ccf105b8eefe414be4b8c06f4803cdb
gabokatta <gkatta@fi.uba.ar>
2025-09-02 20:45:31 -0300

    log batchAmount and batchBytes

--- client/main.go
+++ client/main.go
@@ -80,11 +80,13 @@ func InitLogger(logLevel string) error {
 // PrintConfig Print all the configuration parameters of the program.
 // For debugging purposes only
 func PrintConfig(v *viper.Viper) {
-	log.Infof("action: config | result: success | client_id: %s | server_address: %s | loop_period: %v | log_level: %s",
+	log.Infof("action: config | result: success | client_id: %s | server_address: %s | loop_period: %v | log_level: %s | batch_bytes: %v | batch_amount: %v",
 		v.GetString("id"),
 		v.GetString("server.address"),
 		v.GetDuration("loop.period"),
 		v.GetString("log.level"),
+		v.GetUint32("batch.maxBytes"),
+		v.GetUint32("batch.maxAmount"),
 	)
 }
 
